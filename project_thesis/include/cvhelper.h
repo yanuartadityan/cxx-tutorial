ea127ac754543a2f5d0f81a39ca92ae5aae700fa
Yanuar T. Aditya Nugraha <yanuart.adityan@gmail.com>
2020-04-15 19:26:47 +0200

    added opencv project

---
+++ project_thesis/include/cvhelper.h
@@ -0,0 +1,12 @@
+//
+// Created by Mirkwood Ape on 4/12/2020.
+//
+
+#ifndef CXX_TUTOR_CVHELPER_H
+#define CXX_TUTOR_CVHELPER_H
+
+#include "opencv4/opencv2/opencv.hpp"
+
+void getCvVersion();
+
+#endif //CXX_TUTOR_CVHELPER_H
---
+++ project_thesis/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Mirkwood Ape on 4/12/2020.
+//
+
+#include <iostream>
+#include "opencv4/opencv2/opencv.hpp"
+#include "include/cvhelper.h"
+
+using namespace cv;
+using namespace std;
+
+int main(){
+
+    getCvVersion();
+
+    return 0;
+}
