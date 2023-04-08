4dee73d1f0dc85aac18b0da7c9a07a49abe26c33
Yanuar T. Aditya Nugraha <yanuar.aditya@smarteye.se>
2023-04-08 14:03:44 +0200

    Resize image

--- tutor_opencv/load_image.cpp
+++ tutor_opencv/load_image.cpp
@@ -5,27 +5,51 @@
 
 #include <iostream>
 #include <filesystem>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
 using namespace cv;
 
+#define MAX_PIXEL 640 // pixels
+
+
+void scaled_ratio(int original_width, int original_height, int max_pixel, int *out_width, int *out_height) {
+    // determine the orientation
+    if (original_width > original_height) {
+        // landscape
+        auto ratio = float(original_width) / float(max_pixel);
+        *out_width = max_pixel;
+        *out_height = int(float(original_height) / ratio);
+    } else {
+        auto ratio = float(original_height) / float(max_pixel);
+        *out_width = int(float(original_width) / ratio);
+        *out_height = max_pixel;
+    }
+}
+
 int main(int argc, char* argv[]) {
     std::filesystem::path assets_path = std::filesystem::current_path() / "assets/";
     std::cout << "Loading assets in " << assets_path << std::endl;
-    Mat img = imread(assets_path / "flower.jpeg");
 
-    if (img.empty()) {
+    // Load unscaled image
+    Mat img = imread(assets_path / "flower.jpeg");
+    Mat resized_img;
+    int new_w, new_h;
+    scaled_ratio(img.size().width, img.size().height, MAX_PIXEL, &new_w, &new_h);
+    resize(img, resized_img, Size(new_w, new_h), INTER_LINEAR);
+    
+    if (resized_img.empty()) {
         std::cout << "Could not load the sample image: " << std::endl;
         return 1;
     }
 
-    imshow("Display window", img);
+    imshow("Display window", resized_img);
     int k = waitKey(0);
 
     if (k == 's') {
-        imwrite(assets_path / "flower.jpeg", img);
+        imwrite(assets_path / "flower.jpeg", resized_img);
     }
 
     return 0;
